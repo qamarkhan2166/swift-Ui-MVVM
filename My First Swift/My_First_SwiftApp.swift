ba052615b2fea8a895f641bad79c4f6474a5a3b0
qamarkhan2166 <qamarkhan2166@gmail.com>
2024-12-17 03:24:00 +0400

    Initial Commit

---
+++ My First Swift/My_First_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_First_SwiftApp.swift
+//  My First Swift
+//
+//  Created by Qamar khan on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct My_First_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ My First SwiftTests/My_First_SwiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  My_First_SwiftTests.swift
+//  My First SwiftTests
+//
+//  Created by Qamar khan on 12/17/24.
+//
+
+import Testing
+
+struct My_First_SwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
